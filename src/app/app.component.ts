09498282947837598003c583c21da0380d6b4a9a
Aubacca <rohnerp@emea.roche.com>
2017-05-09 16:57:18 +0200

    Grid added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { MdDialog, MdDialogRef, MdSnackBar, MdInputContainer } from '@angular/ma
 })
 export class AppComponent {
   title = 'app works with newest ng version!';
+
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 }
